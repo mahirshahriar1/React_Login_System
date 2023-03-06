2746d9135f4eac052280e02467bccaa650e573d2
Mahir Shahriar Tamim <03mahirshahriar@gmail.com>
2023-03-07 01:20:16 +0600

    cookies hashing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+
 
 function App() {
 
@@ -13,31 +14,42 @@ function App() {
   const [loginStatus, setLoginStatus] = useState("");
   const [color, setColor] = useState("red");
 
+  Axios.defaults.withCredentials = true;
+
   const register = () => {
     Axios.post('http://localhost:3001/register', {
       username: usernameReg,
       password: passwordReg
     }).then((response) => {
-      console.log(response);
+      alert(response.data.message);
     });
   };
+
   const login = () => {
- 
+
     Axios.post('http://localhost:3001/login', {
       username: username,
       password: password
     }).then((response) => {
-     // console.log(response);
+      // console.log(response);
       if (response.data.message) {
         setLoginStatus(response.data.message);
         setColor("red");
       } else {
-        setLoginStatus(response.data[0].Username+" is logged in");
+        setLoginStatus(response.data[0].Username + " is logged in");
         setColor("green");
       }
     });
   };
 
+  useEffect(() => {
+    Axios.get('http://localhost:3001/login').then((response) => {
+      if (response.data.loggedIn === true) {
+        setLoginStatus(response.data.user[0].Username + " is logged in");
+        setColor("green");
+      }
+    });
+  }, []);
 
 
 
@@ -57,18 +69,18 @@ function App() {
         <h1>Login</h1>
 
         <label> Username </label>
-        <input type="text" placeholder="Username..." 
+        <input type="text" placeholder="Username..."
           onChange={(e) => setUsername(e.target.value)} />
 
         <label > Password </label>
-        <input type="text" placeholder="Password..." 
+        <input type="text" placeholder="Password..."
           onChange={(e) => setPassword(e.target.value)} />
 
         <button onClick={login} >Login</button>
       </div>
 
 
-      <h1 style={{color}}   >{loginStatus}</h1>
+      <h1 style={{ color }}>{loginStatus}</h1>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -3,8 +3,32 @@ const mysql = require('mysql');
 const cors = require('cors');
 const app = express();
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true
+}));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    key: "userId",
+    secret: "makeaverybigsecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24,
+    },
+}));
 
 const db = mysql.createConnection({
     user: 'root',
@@ -17,28 +41,66 @@ app.post('/register', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    db.query("INSERT INTO users (Username, Password) VALUES (?, ?)", [username, password], (err, result) => {
-        console.log(err);
+    db.query("SELECT Username FROM users WHERE Username = ?", [username], (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            res.send({ message: "Username already exists" });
+        } else {
+
+            bcrypt.hash(password, saltRounds, (err, hash) => {
+                if (err) {
+                    console.log(err);
+                }
+                db.query("INSERT INTO users (Username, Password) VALUES (?, ?)", [username, hash], (err, result) => {
+                    console.log(err);
+                });
+            });
+        }
+
     });
+});
+
+
+app.get('/login', (req, res) => {
+    if (req.session.user) {
+        //console.log('logged in');
+        res.send({ loggedIn: true, user: req.session.user });
+    } else {        
+       // console.log('not logged in');
+        res.send({ loggedIn: false });
+    }
 });
 
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-   
-    db.query("SELECT * FROM users WHERE Username = ? AND Password = ?", [username, password], (err, result) => {
-        if (err) {
-            res.send({ err: err });
-        }
-        else {
-            if (result.length > 0) {
-                res.send(result);
-            } else {
-                res.send({ message: "Wrong username/password combination!" });
+
+
+    db.query("SELECT * FROM users WHERE Username = ?;", username,
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
             }
-        }
-    });
+
+            if (result.length > 0) {
+                bcrypt.compare(password, result[0].Password, (error, response) => {
+                    if (response) {
+                        req.session.user = result;
+                       // console.log(req.session.user);                 
+                        res.send(result);
+                    } else {
+                        res.send({ message: "Wrong username/password combination!" });
+                    }
+                });
+            } else {
+                res.send({ message: "User Doesn't Exist" });
+            }
+
+        });
 });
 
 
